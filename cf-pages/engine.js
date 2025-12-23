b68bfd6caa0c11fc0127dc1ec8d6a76c69168285
LUKAIRO <209160725+STR8AI@users.noreply.github.com>
2025-12-23 14:39:20 -0500

    Add navigation links to engine.js

--- cf-pages/engine.js
+++ cf-pages/engine.js
@@ -1,3 +1,11 @@
+<nav class="nav">
+  <a href="/">Home</a>
+  <a href="/pricing.html">Pricing</a>
+  <a href="/engine.html">Engine</a>
+  <a href="/about.html">About</a>
+  <a href="/contact.html">Contact</a>
+</nav>
+
 import { startGlobe } from "./visuals/globe.js";
 import { startPlatforms } from "./visuals/platforms.js";
 
