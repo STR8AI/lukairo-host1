dca13fe02976ad4030722ed5932963f1520c84ca
LUKAIRO <209160725+STR8AI@users.noreply.github.com>
2026-04-15 16:58:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>LUKAIRO</title>
+  <link rel="stylesheet" href="lukairo-main.css" />
   <link rel="stylesheet" href="./style.css" />
 </head>
 <body>
